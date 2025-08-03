6dfa483b4b7a4df0ecb3e5c4dcd1d415176a6625
M.Sankar Reddy <muvvasankar16@gmail.com>
2025-08-03 21:36:44 +0530

    Setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
 
   return (
     <>
-      
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
